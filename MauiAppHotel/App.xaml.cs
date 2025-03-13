bc63f41ff91f00d9663ccc710a1fa8167232d481
sergio <0200832221022@LAB03-01>
2025-03-12 22:02:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public List<Quarto> Lista_quartos = new List<Quarto>()
+        {
+            new Quarto()
+            {
+                Descricao = "Suite Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suite Super Single",
+                ValorDiariaAdulto = 50,
+                ValorDiariaCrianca = 25
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suite Super Crise",
+                ValorDiariaAdulto = 25,
+                ValorDiariaCrianca = 12.5
+            }
+        };//Fecha List<Quarto>]
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+            {
+                var window = base.CreateWindow(activationState);
+                
+                window.Width = 400;
+                window.Height = 600; 
+            
+            return window;
+            }
+        
+       
+        }
+    }
+}
---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+}
